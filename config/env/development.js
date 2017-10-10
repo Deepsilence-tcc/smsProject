4f497d9028ada73e9c0455eb3d9d673bad4ec80f
tovinie <Deepsilence-tcc@users.noreply.github.com>
2017-10-09 21:36:55 -0500

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,4 @@
 ﻿module.exports = {
-    port: 3000,
-    mongodb:'mongodb://learn:learn0228@39.108.79.155/peach',
-    appKey:'LTAIIyPwQRX5A6sP',
-    appSecret:'QefFyqIPBdUdOCcSLCgx0YE8J9qxYY'
-
+  
 
 }
